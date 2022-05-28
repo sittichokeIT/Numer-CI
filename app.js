0e6326a4a23e449ac959fc6bc8ea50dacc935a71
s6204062620046 <s6204062620046@email.kmutnb.ac.th>
2022-05-29 03:45:18 +0700

    deploy

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = process.env.PORT || 5000
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build/index.html'))
+    })
+}
+
+app.listen(port, (err) => {
+    if(err) return console.log(err)
+    console.log('Server running on port: ', port)
+})
